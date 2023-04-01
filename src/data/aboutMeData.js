9c0b821f5eebc42f4d469a9c5d588cd4face2d14
Juan Ignacio Tejada <jtejada@lubee.com.ar>
2023-04-01 14:24:43 -0300

    New changes

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,10 @@
 export const aboutMeData = [
-	{
-		id: 1,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
-	},
-	{
-		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-	},
+  {
+    id: 1,
+    bio: "My name is Juan Ignacio Tejada, I am 27 years old, and I am a Junior Front End Developer with strong problem-solving skills and teamwork abilities, with a lot of ambition to contribute knowledge and learn more about the industry.",
+  },
+  {
+    id: 2,
+    bio: "My skills are in HTML5, CSS3, Sass, JavaScript, Vue.js, React.js, Git/Github, and UX/UI. I also have knowledge in Agile methodologies such as Scrum and Kanban.",
+  },
 ];
